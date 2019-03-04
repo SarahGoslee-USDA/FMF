8404ed52e732e6977afa9bc0a731863e05bf7274
Sarah Goslee <32748053+sgoslee@users.noreply.github.com>
2019-03-04 17:27:50 -0500

    Checked against APEXgraze

--- objects/apexrun.dat.R
+++ objects/apexrun.dat.R
@@ -6,6 +6,6 @@
 apexrun.desc <- make.desc(
     make.section(rep(NA, 7), rfmt=c("c", rep("i", 6)), rdoc="run configuration", rnames=c("ASTN", "ISIT", "IWPM", "IWND", "ISUB", "ISOL", "IRFT"), times=NA, delim=" "),
     terminal = "XXXXXXXX   0   0   0   0   0",
-    doc = "APEX APEXRUN.DAT 2016-05-11")
+    doc = "APEXgraze APEXRUN.DAT 2019-02-07")
 
 
